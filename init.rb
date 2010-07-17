1dcba97bd91c4fa4e37b235dc3dde699dc89dc13
Josh Delsman <jdelsman@voxxit.com>
2010-07-17 01:34:23 -0400

    Make compatible with Rails 3

--- init.rb
+++ init.rb
@@ -1,1 +1,17 @@
-require 'rails/init.rb'
+# The following options can be changed by creating an initializer in config/initializers/jrails.rb
+
+# jRails uses jQuery.noConflict() by default
+# to use the default jQuery varibale, use:
+# ActionView::Helpers::PrototypeHelper::JQUERY_VAR = '$'
+
+# ActionView::Helpers::PrototypeHelper:: DISABLE_JQUERY_FORGERY_PROTECTION
+# Set this to disable forgery protection in ajax calls
+# This is handy if you want to use caching with ajax by injecting the forgery token via another means
+# for an example, see http://henrik.nyh.se/2008/05/rails-authenticity-token-with-jquery
+# ActionView::Helpers::PrototypeHelper::DISABLE_JQUERY_FORGERY_PROTECTION = true
+
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery.min', 'jquery-ui.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jrails => ['jrails.min']
+
+require 'jrails'
+require 'jquery_selector_assertions' if Rails.env.test?
